dae1a5f4c37bba6e98e7a57cfcc511103fc687d6
klodianshaba <klodian.tik@gmail.com>
2018-04-04 23:15:08 +0200

    show category_products

--- Server_PHP/Controllers/Products/Products.php
+++ Server_PHP/Controllers/Products/Products.php
@@ -34,7 +34,7 @@ class Products extends Fetch_Data {
             )
             // more table and columns ............
         );
-
+   // sdfsdfsd
         $crypto = new Crypto();
 
         $decrypted = $crypto->decrypt_in_server( $array_data['type'] );
